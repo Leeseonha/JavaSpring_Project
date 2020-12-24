ace583a74f7b063be670aac2082f2a0a26b3a05f
Leeseonha <jmtkdsh@gmail.com>
2020-12-24 14:14:47 +0900

    POST

---
+++ src/main/java/com/example/study/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.example.study.controller;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    // post는 HTML <Form> , ajax 검색할 때 사용됨
+    // post는 http post 통신을 할 때 body에 data를 집어넣어서 보내겠다라는 뜻이어서 @RequestBody에 값을 매칭함
+    // request할 때 여러가지 값이 있음 // json, xml, multipart-form / text-plain
+
+    // @RequestMapping(method = RequestMethod.POST , path = "/postMethod") =>아래 PostMapping과 동일한 동작
+    @PostMapping("/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+
+    @PutMapping("/putMethod")
+    public void put(){
+
+    }
+
+    @PatchMapping("/patchMethod")
+    public void patch(){
+        
+    }
+
+}
