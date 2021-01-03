dce2bf261639a0d8a861d9cc79b14a816c68e147
Leeseonha <jmtkdsh@gmail.com>
2021-01-03 15:18:16 +0900

    Lombok

--- mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,10 +1,18 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Person {
 
     @Id
@@ -15,36 +23,30 @@ public class Person {
 
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDateTime birthday;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    // @ToString.Exclude : 결과창에 개인정보라 로그에 남으면 안될 때 쓰면 결과창에 안보임
+    private String phoneNumber;
 
-    @Override
-    public String toString(){
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+//    @Override
+//    public String toString(){
+//        return "Person{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                ", hobby='" + hobby + '\'' +
+//                ", bloodType='" + bloodType + '\'' +
+//                ", address='" + address + '\'' +
+//                ", birthday='" + birthday + '\'' +
+//                ", job='" + job + '\'' +
+//                '}';
+//    }
 }
