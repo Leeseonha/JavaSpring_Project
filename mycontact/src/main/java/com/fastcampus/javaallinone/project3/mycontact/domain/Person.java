f16cc4853568f139da1c31b0c76e95d56d4dfa42
Leeseonha <jmtkdsh@gmail.com>
2021-01-03 15:49:29 +0900

    Lombok_완료

--- mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ mycontact/src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,8 +1,6 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,17 +8,24 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-@ToString
+//@Getter
+//@Setter
+//@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+//@EqualsAndHashCode
+@Data
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
     private String hobby;
@@ -36,6 +41,30 @@ public class Person {
     // @ToString.Exclude : 결과창에 개인정보라 로그에 남으면 안될 때 쓰면 결과창에 안보임
     private String phoneNumber;
 
+    // 밑에 코드 대신 @EqualsAndHashCode을 한줄 쓰면 됨
+    // 같은지 비교하는 코드
+//    public boolean equals(Object object){
+//        if(object == null){
+//            return false;
+//        }
+//
+//        Person person = (Person) object;
+//
+//        if(!person.getName().equals(this.getName())){
+//            return false;
+//        }
+//
+//        if(person.getAge() != this.getAge()){
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    public int hashCode(){
+//        return (name+age).hashCode();
+//    }
+
+    // 밑에 코드 대신 @ToString을 한줄 쓰면 됨
 //    @Override
 //    public String toString(){
 //        return "Person{" +
