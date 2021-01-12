1e1c8ddaac885eb4c0c0c410795ae5e49ba15251
Leeseonha <jmtkdsh@gmail.com>
2021-01-12 22:25:28 +0900

    Mock-Test_ServiceTest1

--- mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ mycontact/src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -4,7 +4,12 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 //import com.fastcampus.javaallinone.project3.mycontact.repository.BlockRepository;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,18 +18,24 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+//
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
-    @Autowired
+//    @Autowired
+    @InjectMocks
     private PersonService personService;
 
-    @Autowired
+//    @Autowired
+    @Mock
     private PersonRepository personRepository;
 
     @Test
     void getPeopleByName(){
+        when(personRepository.findByName("martin"))
+                .thenReturn(Lists.newArrayList(new Person("martin")));
 
         List<Person> result = personService.getPeopleByName("martin");
 
