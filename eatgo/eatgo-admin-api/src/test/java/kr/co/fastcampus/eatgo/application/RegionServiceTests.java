6ea40bbff4dfb8527eade992cdb5f0e58d790d50
Leeseonha <jmtkdsh@gmail.com>
2021-02-14 01:41:42 +0900

    가게 목록 필터링1,2

---
+++ eatgo/eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/application/RegionServiceTests.java
@@ -0,0 +1,44 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.Region;
+import kr.co.fastcampus.eatgo.domain.RegionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+public class RegionServiceTests {
+
+    private RegionService regionService;
+
+    @Mock
+    private RegionRepository regionRepository;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        regionService = new RegionService(regionRepository);
+    }
+
+    @Test
+    public void getRegions(){
+        List<Region> mockregions = new ArrayList<>();
+        mockregions.add(Region.builder().name("Seoul").build());
+
+        given(regionRepository.findAll()).willReturn(mockregions);
+
+        List<Region> regions = regionService.getRegions();
+
+        Region region = regions.get(0);
+        assertThat(region.getName(), is("Seoul"));
+    }
+
+}
