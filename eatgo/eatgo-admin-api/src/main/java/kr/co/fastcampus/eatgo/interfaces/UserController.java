3250cd614a98bfbb65813355590aa954b6e04626
Leeseonha <jmtkdsh@gmail.com>
2021-02-19 18:49:01 +0900

    사용자 관리1

---
+++ eatgo/eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/UserController.java
@@ -0,0 +1,29 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.UserService;
+import kr.co.fastcampus.eatgo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> list(){
+        List<User> users = userService.getUsers();
+
+        return users;
+    }
+
+    // 1. User list
+    // 2. User create -> 회원가입
+    // 3. User update
+    // 4. User delete -> 실제로 삭제하지 않고 level:0 으로 설정하여 아무것도 못 하게 한다.
+    // (level 1: customer, 2: restaurant owner, 3: admin)
+}
---
+++ eatgo/eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/UserTests.java
@@ -0,0 +1,22 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTests {
+
+    @Test
+    public void creation(){
+        User user = User.builder()
+                .email("tester@example.com")
+                .name("Tester")
+                .level(100L)
+                .build();
+
+        assertThat(user.getName(), is("Tester"));
+        assertThat(user.isAdmin(), is(true));
+    }
+
+}
