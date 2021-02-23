39781f082279dbf2c5715dcfef17e22b5bc7cb4f
Leeseonha <jmtkdsh@gmail.com>
2021-02-23 13:29:03 +0900

    회원가입1

---
+++ eatgo/eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/SecurityJavaConfig.java
@@ -0,0 +1,20 @@
+package kr.co.fastcampus.eatgo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()   // 보안에서 중요한데, 현재 REST-API를 만들 때 API서버에서 불필요하기 때문에 disable시킴.
+                .csrf().disable()   // 보안에서 중요한데, 현재 REST-API를 만들 때 API서버에서 불필요하기 때문에 disable시킴.
+                .formLogin().disable()
+                .headers().frameOptions().disable();
+    }
+}
