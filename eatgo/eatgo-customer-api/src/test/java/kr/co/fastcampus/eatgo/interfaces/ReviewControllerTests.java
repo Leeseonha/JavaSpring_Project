b5f7c475930f0fa598d74518c42220f817d1d27f
Leeseonha <jmtkdsh@gmail.com>
2021-03-05 12:37:59 +0900

    인가2

--- eatgo/eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/ReviewControllerTests.java
+++ eatgo/eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/ReviewControllerTests.java
@@ -33,17 +33,20 @@ public class ReviewControllerTests {
 
     @Test
     public void createWithValidAttriutes() throws Exception {
-        given(reviwService.addReview(eq(1L), any())).willReturn(Review.builder()
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwMDQsIm5hbWUiOiJKb2huIn0.8hm6ZOJykSINHxL-rf0yV882fApL3hyQ9-WGlJUyo2A";
+
+        given(reviwService.addReview(1L, "John", 3, "Mat-it-da")).willReturn(Review.builder()
                 .id(1004L)
                 .build());
 
         mvc.perform(post("/restaurants/1/reviews")
+                .header("Authorization", "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"JOKER\",\"score\":3,\"description\":\"Mat-it-da\"}"))
+                .content("{\"score\":3,\"description\":\"Mat-it-da\"}"))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("location", "/restaurants/1/reviews/1004" ));
 
-        verify(reviwService).addReview(eq(1l), any());
+        verify(reviwService).addReview(eq(1l), eq("John"), eq(3), eq("Mat-it-da"));
     }
 
     @Test
@@ -53,7 +56,7 @@ public class ReviewControllerTests {
                 .content("{}"))
                 .andExpect(status().isBadRequest());
 
-        verify(reviwService, never()).addReview(eq(1L), any());
+        verify(reviwService, never()).addReview(any(), any(), any(), any());
     }
 
 }
