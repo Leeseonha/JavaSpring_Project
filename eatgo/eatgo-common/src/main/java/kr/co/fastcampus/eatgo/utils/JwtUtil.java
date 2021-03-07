17be77b5ad261d8240e4f62b2452b9e35e4eac59
Leeseonha <jmtkdsh@gmail.com>
2021-03-07 20:35:56 +0900

    로그인 API 분리_2

--- eatgo/eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo/eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
@@ -1,10 +1,10 @@
 package kr.co.fastcampus.eatgo.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
@@ -16,14 +16,17 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
-    public String createToken(Long userId, String name) {
-        String token = Jwts.builder()
+    public String createToken(Long userId, String name, Long restaurantId) {
+        JwtBuilder builder = Jwts.builder()
                 .claim("userId", userId)
-                .claim("name", name)
+                .claim("name", name);
+        if (restaurantId != null){
+            builder = builder.claim("restaurantId", restaurantId);
+        }
+
+        return builder
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
-
-        return token;
 //        return "header.payload.signature";
     }
 
